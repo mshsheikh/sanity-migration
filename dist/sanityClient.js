a831dba914eab04da0613a7ba8a94704e337ca90
mshsheikh <business.mshsheikh@example.com>
2025-01-12 13:06:40 +0500

    boilerplate+cms

--- dist/sanityClient.js
+++ dist/sanityClient.js
@@ -4,9 +4,9 @@ exports.client = void 0;
 // sanityClient.ts
 const client_1 = require("@sanity/client");
 exports.client = (0, client_1.createClient)({
-    projectId: 'rohwei6u', // Replace with your project ID
+    projectId: "cturxqmc", // Replace with your project ID
     dataset: 'production', // Or your dataset name
     apiVersion: '2024-01-04', // Today's date or latest API version
     useCdn: false, // Disable CDN for real-time updates
-    token: "skwAYL2EVT699VvWODU6xcibH3o0mwbKP9DoMrexRkfzRpaWfQahfSiwEuAlwZqU4FibHyEoVOR8gmHnwnDXW640gZU3mp1afdgil0yTqqL9Oqw4pqTssSZERXnLbXopBPVqpiW0OVN7hTAH0fF7GbSjAKDDk9kbYAAZZpDfdWhYAHWmtFAY"
+    token: "skX6JGp5Jn4UvUh1CrMTyOo0ll6JCYddLeuEQvvNQh64HtGhLh5hKiAwGLNxwAog9Yz4rnfPj3ayd6Xt3V9I5sZnmjRAsuDjvCBIdSwCBE7JT426MBs8jast7XjV5g5Q3qB0XlNgMraD9ygL9lcz0e9xWQonixXaxIjdVv0J1jLxfzLMB0sb",
 });
--- sanityClient.ts
+++ sanityClient.ts
@@ -2,9 +2,9 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.projectId, // Replace with your project ID
+  projectId: "cturxqmc", // Replace with your project ID
   dataset: 'production',        // Or your dataset name
   apiVersion: '2024-01-04',     // Today's date or latest API version
   useCdn: false,                // Disable CDN for real-time updates
-  token: process.env.token,
+  token: "skX6JGp5Jn4UvUh1CrMTyOo0ll6JCYddLeuEQvvNQh64HtGhLh5hKiAwGLNxwAog9Yz4rnfPj3ayd6Xt3V9I5sZnmjRAsuDjvCBIdSwCBE7JT426MBs8jast7XjV5g5Q3qB0XlNgMraD9ygL9lcz0e9xWQonixXaxIjdVv0J1jLxfzLMB0sb",
 });
